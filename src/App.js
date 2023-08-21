07be3a61ec97fc56f0a3bc5cb4447288006cfd37
unknown <mohandubeyang@gmail.com>
2023-08-21 18:13:10 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import Hero from "./pages/app/hero/Hero";
+import Nav from "./components/nav/Nav";
+import Courses from "./pages/app/courses/Courses";
+import Learn from "./pages/app/learn/Learn"
+import Chapter from "./pages/app/chapter/Chapter";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Details from "./pages/app/details/Details";
+
+
+
+function App() {
+  const browerRouter = createBrowserRouter([
+    {
+      path: '/',
+      element: <Nav/>,
+      children: [
+        {index:true , element: <Hero/>},
+        {
+          path: '/courses',
+          children: [
+            {index: true, element:<Courses/>},
+            {
+              path:':courseId',
+              element: <Details/>
+            }
+          ]
+        },
+        {
+          path: 'learn/:courseId',
+          element: <Learn/>,
+          children:[
+            {
+               path: 'chapter/:chapterId',
+               element: <Chapter/>
+            }
+
+          ]
+        }
+      ]
+    }
+  ])
+  return (
+    <RouterProvider router = {browerRouter}/>
+  );
+}
+
+export default App;
