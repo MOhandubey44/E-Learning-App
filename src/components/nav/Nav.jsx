bbfb1d25e69248ce92b458e0269b92875fad06d5
Mohan Dubey <mohandubeyang@gmail.com>
2024-02-10 12:16:06 +0530

    changed

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -5,31 +5,29 @@ import { Link, NavLink } from "react-router-dom";
 function Nav() {
   return (
     <>
-    <nav>
-      <div className={style.nav_container}>
-        <div className={style.nav_title_wrapper}>
-          <Link to = '/'>
-            <img
-              className={style.logo}
-              src="https://files.codingninjas.in/pl-ninja-16706.svg"
-              alt="logo"
-            />
-          </Link>
-          <h4>Coding Ninjas</h4>
-        </div>
-        <div className={style.nav_details}>
-
-          <button>
+      <nav>
+        <div className={style.nav_container}>
+          <div className={style.nav_title_wrapper}>
+            <Link to="/">
+              <img
+                className={style.logo}
+                src="https://files.codingninjas.in/pl-ninja-16706.svg"
+                alt="logo"
+              />
+            </Link>
+            <h4>Coding Ninjas</h4>
+          </div>
+          <div className={style.nav_details}>
+            <button>
               <NavLink to="/courses">
-                {({isActive})=>(isActive?'On-Courses': "Courses")}
+                {({ isActive }) => (isActive ? "On-Course" : "Courses")}
               </NavLink>
-          </button>
+            </button>
+          </div>
         </div>
-      </div>
-    </nav>
-    <Outlet/>
+      </nav>
+      <Outlet />
     </>
-    
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import TagManager from "react-gtm-module";
 
+const tagManagerArgs = {
+  gtmId: "GTM-PHCLBXFN",
+};
+
+TagManager.initialize(tagManagerArgs);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-      <App />
+    <App />
   </React.StrictMode>
 );
